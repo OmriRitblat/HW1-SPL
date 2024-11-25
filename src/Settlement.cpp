6c0014113fc08c3e73d58947c1afe37321546724
adicohen <adia5@post.bgu.ac.il>
2024-11-25 20:22:40 +0200

    create settelement cpp

---
+++ src/Settlement.cpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <vector>
+using std::string;
+using std::vector;
+
+Settlement::Settlement(const string &name, SettlementType type):name(name),type(type){
+}
