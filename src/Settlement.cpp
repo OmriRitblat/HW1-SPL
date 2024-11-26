380bfdb455d84da9295a0b6a4fa03e5ce1d9cc1a
adicohen <adia5@post.bgu.ac.il>
2024-11-26 19:16:09 +0200

    file creation

--- src/Settlement.cpp
+++ src/Settlement.cpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <string>
 #include <vector>
+#include "Settlement.h"
 using std::string;
 using std::vector;
+
 
 Settlement::Settlement(const string &name, SettlementType type):name(name),type(type){
 }
